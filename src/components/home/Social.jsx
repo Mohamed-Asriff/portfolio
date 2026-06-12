b7c01fa68368cb557b5af7cb120a4a219c39fee9
Mohamed-Asriff <mohamedasriff2003@gmail.com>
2026-06-12 18:07:51 +0530

    fix eslint warnings for deployment

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import "./social.css";
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.linkedin.com/in/mohamed-asriff-122as315/" className="home__social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/mohamed-asriff-122as315/" className="home__social-icon" target="_blank" rel="noreferrer">
       <i class='bx bxl-linkedin' ></i>
       </a>
 
-      <a href="https://twitter.com/AsriffMohamed" className="home__social-icon" target="_blank">
+      <a href="https://twitter.com/AsriffMohamed" className="home__social-icon" target="_blank" rel="noreferrer">
         <i class="uil uil-twitter"></i>
       </a>
 
-      <a href="https://github.com/Mohamed-Asriff" className="home__social-icon" target="_blank">
+      <a href="https://github.com/Mohamed-Asriff" className="home__social-icon" target="_blank" rel="noreferrer">
         <i class="uil uil-github-alt"></i>
       </a>
     </div>
